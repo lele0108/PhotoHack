a8718666d82f39f72c5c93969012a21cb23388ed
Jimmy Liu <sicong.liu98@gmail.com>
2013-04-06 14:35:40 -0700

    PHP changes

---
+++ footer.php
@@ -0,0 +1,19 @@
+<div class="footer">
+      <div class="row-fluid">
+
+        <div class="span5 offset1">
+          <p>Created at PhotoHack Day 2013</p>
+        </div>
+        <div class="span5">
+          <p class="text-right">Copyright 2013</p>
+        </div>
+
+      </div>
+    </div>
+
+
+
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>iMaze - Mazes from Photos</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href="css/custom.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+    <div class="menu">
+    	<div class="row-fluid">
+    		<div class="span10 offset1">
+				<div class="row-fluid">
+					<div class="span3">
+						<p>iMaze</p>
+					</div>
+					<div class="span3 offset6">
+						<p class="text-right"><a href="#">HELLO</a> <a href="#">GENERATE</a> <a href="#">ABOUT</a></p>
+					</div>
+				</div>
+    		</div><!--end span10 offset1!-->
+    	</div><!--end row fluid!-->
+      </div><!--end menu!-->
