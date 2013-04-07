ee65898bc262d367eac63ed8f67378ed6233b5c7
Zbynek Nedoma <zbynek.nedoma@gmail.com>
2013-04-07 00:59:17 -0700

    500px

--- api/500px.php
+++ api/500px.php
@@ -2,14 +2,14 @@
 <html>
   <head>
     <title>500px SDK Example 1</title>
-    <script src="../js/500px.js"></script>
+    <script src="/500px.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
   </head>
   <body>
     <script>
       function searchNext(){
           _500px.api('/photos/search', { term: $("#searchInput").val(), rpp: 5, page: $("#page").val(), user_id: 3007733 }, function (response) {
-              console.log(response);
+              $("#logged_in").empty();
               $.each(response.data.photos, function () {
                 $('#logged_in').append('<img src="' + this.image_url + '" />');
               });
@@ -20,7 +20,7 @@
       function searchNew(){
     	  $("#page").val(1);
     	  _500px.api('/photos/search', { term: $("#searchInput").val(), rpp: 5, page: $("#page").val(), user_id: 3007733 }, function (response) {
-              console.log(response);
+    		  $("#logged_in").empty();
               $.each(response.data.photos, function () {
                 $('#logged_in').append('<img src="' + this.image_url + '" />');
               });
@@ -34,17 +34,9 @@
         });
       });
     </script>
-
-    <h1>Your 500px favorite photos</h1>
-
-    <?php echo $_GET['term']; ?>
-
       <input id="searchInput" type="text" name="term">
       <div onclick="searchNew()">Search</div>
-    <div id="not_logged_in">
-      <a href="#" id="login">Login to 500px</a>
-    </div>
-    <div id="logged_in" style="display: none;">
+    <div id="logged_in">
     </div>
     <div id="" style="" onclick="searchNext()">Search Next
     </div>
