c8e6db046c872a42f46086faaecf60dd8a09e842
Zbynek Nedoma <zbynek.nedoma@gmail.com>
2013-04-07 00:50:49 -0700

    500px

--- api/500px.php
+++ api/500px.php
@@ -8,17 +8,22 @@
   <body>
     <script>
       function searchNext(){
-        $("#logged_in").empty();
-        
-        _500px.api('/users', function (response) {
-            var me = response.data.user;
-            // Get my favorites
-            _500px.api('/photos/search', { term: '<?php echo $_GET['term']; ?>', rpp: 5, page: $("#page").val(), user_id: me.id }, function (response) {
-                console.log(response);
-                $.each(response.data.photos, function () {
-                  $('#logged_in').append('<img src="' + this.image_url + '" />');
-                });
-            });
+          _500px.api('/photos/search', { term: $("#searchInput").val(), rpp: 5, page: $("#page").val(), user_id: 3007733 }, function (response) {
+              console.log(response);
+              $.each(response.data.photos, function () {
+                $('#logged_in').append('<img src="' + this.image_url + '" />');
+              });
+          });
+          $("#page").val(parseInt($("#page").val())+1); 
+      }
+
+      function searchNew(){
+    	  $("#page").val(1);
+    	  _500px.api('/photos/search', { term: $("#searchInput").val(), rpp: 5, page: $("#page").val(), user_id: 3007733 }, function (response) {
+              console.log(response);
+              $.each(response.data.photos, function () {
+                $('#logged_in').append('<img src="' + this.image_url + '" />');
+              });
           });
           $("#page").val(parseInt($("#page").val())+1); 
       }
@@ -27,37 +32,6 @@
         _500px.init({
           sdk_key: '9920bb2b69c7f071b25edeb643cc70d9c98373cc'
         });
-          
-// When the user logs in we will pull their favorite photos
-        _500px.on('authorization_obtained', function () {
-          $('#not_logged_in').hide();
-          $('#logged_in').show();
-
-          // Get my user id
-          _500px.api('/users', function (response) {
-            var me = response.data.user;
-            // Get my favorites
-            _500px.api('/photos/search', { term: '<?php echo $_GET['term']; ?>', rpp: 5, page: $("#page").val(), user_id: me.id }, function (response) {
-                console.log(response);
-                $.each(response.data.photos, function () {
-                  $('#logged_in').append('<img src="' + this.image_url + '" />');
-                });
-                $("#page").val(parseInt($("#page").val())+1);
-            });
-          });
-        });
-
-       _500px.on('logout', function () {
-          $('#not_logged_in').show();
-          $('#logged_in').hide();
-          $('#logged_in').html('');
-        });
-
-        // If the user has already logged in & authorized your application, this will fire an 'authorization_obtained' event
-        _500px.getAuthorizationStatus();
-
-        // If the user clicks the login link, log them in
-        $('#login').click(_500px.login);
       });
     </script>
 
@@ -65,10 +39,8 @@
 
     <?php echo $_GET['term']; ?>
 
-    <form name="search" id="search">
-      <input type="text" name="term">
-      <button type="submit">Search</button>
-    </form>
+      <input id="searchInput" type="text" name="term">
+      <div onclick="searchNew()">Search</div>
     <div id="not_logged_in">
       <a href="#" id="login">Login to 500px</a>
     </div>
