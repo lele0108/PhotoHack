e6f64b6a9ffb8cc3a69f379e824f5f66ba0d1158
boris.suska <boris.suska@gmail.com>
2013-04-07 10:47:32 -0700

    merge maze.html and maze.php

--- maze.php
+++ maze.php
@@ -2,48 +2,122 @@
   include_once "header.php";
 ?>
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
-  <script src="../js/imaze.js" type="text/javascript"></script>
-<?php $_SESSION['to_maze'] = $_GET['mazeImage']; ?>	
+  <script type="text/javascript" src="./js/imaze.js"></script>
+  <script type="text/javascript" src="./js/canvas-touch.js"></script>
+  <script type="text/javascript" src="./js/URI.js"></script>
 
   <script type="text/javascript">
   //<!--
-    $(window).load(function() {
-      var canvas = document.getElementById('maze');
-      var ctx = canvas.getContext("2d");
-      var wPixel = ctx.createImageData(1,1);
-      wPixel.data[0] = 255; wPixel.data[1] = 255; wPixel.data[2] = 255; wPixel.data[3] = 1;
-      var bPixel = ctx.createImageData(1,1);
-      bPixel.data[0] = 0; wPixel.data[1] = 0; bPixel.data[2] = 0; bPixel.data[3] = 1;
-      
-      var img = document.getElementById('user-image');
-      
-      var imaze = maze(45, 80);
-      var iscale = 7;
-      var ibitmap = scale(bitmap(imaze), iscale);
-      
-      ctx.fillRect( x, y, 1, 1 );
-      
-      var width = ibitmap[0].length;
-      var height = ibitmap.length;
-      canvas.width = width;
-      canvas.height = height;
-      ctx.drawImage(img, 0, 0, width, height);
-      
-      for (var y=0; y<height; y++) {
-        for (var x=0; x<width; x++) {
-          if (ibitmap[y][x] == 1) {
-            // black
-            // ctx.fillStyle = "rgba("+0+","+0+","+0+","+1+")";
+  $(document).ready(function() {
+    var canvas = document.getElementById('maze');
+    var ctx = canvas.getContext("2d");
+
+    var token = window.location.href.match(/token=([^\.#$\[\]]+)/);
+    if (token) {
+        token = token[1];
+        loadMaze(token, function (snapshot) {
+          if (snapshot.name() == 'data') {
+        	  var data = JSON.parse(snapshot.val());
+             
+            // load image;
+            $('#user-image').attr('src', data.url);
+            // bitmap (global)
+            pixSize = data.scale;
+            ibitmap = scale(bitmap(data.maze), pixSize);
+            
+            // initialize moving      
+            initializeCanvas(canvas, ctx, [255,0,0,255], token);
+            var $readyBtn = $('<a href="#" class="button">I\'m ready!</a>').click(function() {
+              setReady(token, "player2", true);
+            });
+            $('.buttons').html($readyBtn);
+            $readyBtn = null;
+          }
+          else if (snapshot.name() == 'ready') {
+            var ready = snapshot.val();
+            if (ready.player2) {
+              $('.buttons').html('<span>Waiting for player 1...</span>');
+              if (ready.player1) {
+                $('.buttons').hide();
+                // starting count down shuld goes here
+                showMaze(ibitmap, pixSize);
+              }
+            }
+          }
+      	});
+    }
+    else {
+        $('#single-player').click(function() {
+          initializeGame(false);
+        }); 
+        $('#competition').click(function() {
+          initializeGame(true);
+        }); 
+
+        // meze size
+        var imaze = maze(15, 15);
+        var iscale = 20; pixSize = iscale;
+        var ibitmap = scale(bitmap(imaze), iscale);
+        
+        function initializeGame(multiplayer) {
+          if (multiplayer) {
+            var token = requestMultiplayerGame($('#user-image').attr('src'), imaze, iscale, function(snapshot) {
+              if (snapshot.name() == 'ready') {  
+                var ready = snapshot.val();
+                if (ready.player1) {
+                  $('.buttons').html('<span>Waiting for player 2...</span>');
+                  if (ready.player2) {
+                    $('.buttons').hide();
+                    // starting count down shuld goes here
+                    showMaze(ibitmap, iscale);
+                  }
+                } 
+              }
+            });
+            
+            // initialize moving      
+            initializeCanvas(canvas, ctx, [0,255,0,255], token);
+            $('.buttons').html('<label for="url">Send the following URL to your opponent<br></label><input id="url" type="text" value="' + URI(window.location.href).search('token='+token) + '"</span><br><br>');
+            var $readyBtn = $('<a href="#" class="button">I\'m ready!</a>').click(function() {
+              setReady(token, "player1", true);
+            });
+            $('.buttons').append($readyBtn);
+            $readyBtn = null;
           }
           else {
-            // White
-            ctx.fillStyle = "rgba("+255+","+255+","+255+","+1+")";
-            ctx.fillRect( x, y, 1, 1 );
+            // initialize moving      
+            initializeCanvas(canvas, ctx);
+            $('.buttons').hide();
+            // show maze immediately
+            showMaze(ibitmap, iscale);
           }
-        }        
+        }
       }
+        
+      function showMaze(ibitmap, iscale) {
+        var width = ibitmap[0].length;
+        var height = ibitmap.length;
+        canvas.width = width;
+        canvas.height = height;
+  
+        for (var y=0; y<height; y++) {
+          for (var x=0; x<width; x++) {
+            if (ibitmap[y][x] != 1) {
+              // White
+              ctx.fillStyle = "#fff";
+              ctx.fillRect( x, y, 1, 1 );
+            }
+          }        
+        }
       
-    });
+        // Starting point
+        ctx.fillStyle = "#0f0";
+        ctx.fillRect( 0, iscale, iscale, iscale );
+        // destination point
+        ctx.fillStyle = "#f00";
+        ctx.fillRect( width-iscale, height-(2*iscale), iscale, iscale );
+      }
+  });
   //-->
   </script>
 <body>
@@ -52,7 +126,7 @@
         <div class="span10 offset1">
         <div class="row-fluid">
           <div class="span3">
-            <img src="/img/logo.png" width="50%" height="50%">
+            <img src="./img/logo.png" width="50%" height="50%">
           </div>
           <div class="span3 offset6">
             
@@ -75,22 +149,22 @@
   <div class="maze">
   <div class="row">
   <div class="span10 offset1">
-  <div id="mazehere">
-    <canvas id="maze" width="100" height="100"></canvas>
+    <div class="maze-wrap">
+      <div class="buttons">
+        <a id="single-player" class="button" href="#">Sigle player</a>
+        <a id="competition" class="button" href="#">Competition</a>
+      </div>
+      <canvas id="maze"></canvas>
+      <?php if (isset($_GET['mazeImage'])): ?>
+        <img id="user-image" src="<?php echo $_GET['mazeImage']; ?>" />
+      <?php else: ?>
+        <!-- Logo here! -->
+        <img id="user-image" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" />
+      <?php endif; ?>
+    </div>
   </div>
   </div>
-  </div>
-  </div>
-  <div style="display: none;">
-    <img id="user-image" src="<?php echo $_GET['mazeImage']; ?>" />
-  </div>
-  <input id="button" type="submit" name="button" value="enter" onclick="myFunction();"/>
-  <script>
-    document.getElementById('button').onclick = function() {
-   alert("button was clicked");
-}​;​
-  </script>
-
+  </div>  
 <?php
   include_once "footer.php";
 ?>
