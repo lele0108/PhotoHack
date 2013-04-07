d1ea02e36c1e39f9c919a68eb38064be73e14101
Jimmy Liu <sicong.liu98@gmail.com>
2013-04-07 02:51:01 -0700

    Options.php

---
+++ options.php
@@ -0,0 +1,63 @@
+<?php
+  include_once "header.php";
+?>
+    <body>
+
+    <div class="menu">
+      <div class="row-fluid">
+        <div class="span10 offset1">
+        <div class="row-fluid">
+          <div class="span3">
+            <img src="img/logo.png" width="50%" height="50%">
+          </div>
+          <div class="span3 offset6">
+            <p class="text-right"><a href="index.php">HELLO</a> <a href="upload.php"><b>GENERATE</b></a> <a href="about.php">ABOUT</a></p>
+          </div>
+        </div>
+        </div><!--end span10 offset1!-->
+      </div><!--end row fluid!-->
+      </div><!--end menu!-->
+
+      <div class="upload">
+        <div class="row-fluid">
+          <div class="span12">
+            <div class="row-fluid">
+              <div class="span10 offset1">
+              <center><img src="http://placehold.it/800x300" style="border-radius:0px"></center>
+               </div>
+              </div>
+            </div>
+          </div>
+        </div><!--end row fluid!-->
+
+        <div class="options">
+          <div class="row">
+            <div class="span10 offset1">
+              <div class="row-fluid">
+              <div class="text-center">
+
+                <div class="span3"><p>option 1</p>
+                </div>
+
+                <div class="span3"><p>option 2</p>
+                </div>
+
+                <div class="span3"><p>option 3</p>
+                </div>
+
+                <div class="span3"><p>option 4</p>
+                </div>
+
+                </div>
+              </div>
+              <center><br><br><br><a class="awesome-button" href="upload.php">Play Now!</a></center><br>
+            </div>
+          </div>
+      </div>
+
+
+
+
+<?php
+   include_once "footer.php";
+?>
